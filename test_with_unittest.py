21f0c716418a15c4ff6d60492f70eed84b2415b7
che <cndipowa@users.noreply.github.com>
2020-11-26 09:37:38 -0500

    Update test_with_unittest.py

--- test_with_unittest.py
+++ test_with_unittest.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
 
 class TryTesting(TestCase):
-    def test_always_passes(self):
+    def test_always_passes1(self):
         self.assertTrue(True)
 
-    def test_always_fails(self):
-        self.assertTrue(False)
+    def test_always_passes2(self):
+        self.assertTrue(True)
+
+    def test_always_passes3(self):
+        self.assertTrue(True)        
+                
