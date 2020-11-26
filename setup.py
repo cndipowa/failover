a440564ac7fbfcc5b5c00d12e7a30ef618b8782b
che <cndipowa@users.noreply.github.com>
2020-11-26 12:27:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
    name='failover',
    version='1.0',
    description='Datacenter failover',
-   author='Man Foo',
-   packages=['failover'], 
+   py_modules=['failover'], 
    install_requires=['dnspython', 'python-consul'],
    )
